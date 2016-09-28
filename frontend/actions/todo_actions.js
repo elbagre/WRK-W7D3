ad778fdd47625078c2110a12de9e022a184bbfab
Fannie & Peter <pmckinleyiii@gmail.com>
2016-09-28 17:40:06 -0400

    ATTEMPTED PHASE 4

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -9,3 +9,16 @@ export const receiveTodos = todos => ({
   type: RECEIVE_TODOS,
   todos
 });
+
+export const CREATE_TODO = "CREATE_TODO";
+
+export const RECEIVE_TODO = "RECEIVE_TODO";
+
+export const createTodo = () => ({
+  type: CREATE_TODO
+});
+
+export const receiveTodo = (todo) => ({
+  type: RECEIVE_TODO,
+  todo
+});
--- frontend/middleware/todo_middleware.js
+++ frontend/middleware/todo_middleware.js
@@ -1,15 +1,25 @@
 import { React } from 'react';
-import { REQUEST_TODOS, RECEIVE_TODOS, receiveTodos } from '../actions/todo_actions.js';
-import { fetchTodos } from '../util/todo_api_util.js';
+import {
+  REQUEST_TODOS,
+  RECEIVE_TODOS,
+  receiveTodos,
+  CREATE_TODO,
+  RECEIVE_TODO
+} from '../actions/todo_actions.js';
+import { fetchTodos, createTodo } from '../util/todo_api_util.js';
 
 
-export const TodoMiddleware = ({ dispatch }) => (next) => (action) => {
+export const TodoMiddleware = ({ getState, dispatch }) => (next) => (action) => {
   switch(action.type) {
     case REQUEST_TODOS:
       const success = (data) => dispatch(receiveTodos(data));
       const error = e => console.log(e);
       fetchTodos(success, error);
       break;
+    case CREATE_TODO:
+      const success = (data) => receiveTodo(data);
+      const error = e => console.log(e);
+      createTodo(action.todo, success, error);
     default:
       return next(action);
   }
